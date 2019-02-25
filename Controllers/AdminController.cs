8a206696d30918dd1dcf26a3f4a10148203cd08c
Brian Bansenauer <brianb@cascadia.edu>
2019-02-24 20:48:59 -0800

    intial

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IndyBooks.Models;
+using IndyBooks.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IndyBooks.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IndyBooksDataContext _db;
+        public AdminController(IndyBooksDataContext db) { _db = db; }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Search(SearchViewModel search)
+        {
+            //Full Collection Search
+            IQueryable<Book> foundBooks = _db.Books; // start with entire collection
+
+            //Partial Title Search
+            if (search.Title != null)
+            {
+                foundBooks = foundBooks
+                             .Where(b => b.Title.Contains(search.Title))
+                             .OrderBy(b => b.Title)
+                             ;
+            }
+
+            //Author's Last Name Search
+            if (search.AuthorLastName != null)
+            {
+                //TODO:Update to use the Name property of the Book's Author entity
+                foundBooks = foundBooks
+                             .Where(b => b.Author.EndsWith(search.AuthorLastName, StringComparison.CurrentCulture))
+                             ;
+            }
+            //Priced Between Search (min and max price entered)
+            if (search.MinPrice > 0 && search.MaxPrice > 0)
+            {
+                foundBooks = foundBooks
+                             .Where(b => b.Price >= search.MinPrice && b.Price <= search.MaxPrice)
+                             .OrderByDescending(b => b.Price)
+                             ;
+            }
+            //Highest Priced Book Search (only max price entered)
+            if (search.MinPrice == 0 && search.MaxPrice > 0)
+            {
+                decimal max = _db.Books.Max(b => b.Price);
+                foundBooks = foundBooks
+                             ;
+
+            }
+            //Composite Search Results
+            return View("SearchResults", foundBooks);
+        }
+    }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace IndyBooks.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string SKU { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        //TODO: Add Navigtion Property according to ERD Diagram
+
+
+    }
+}
